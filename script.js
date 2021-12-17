a831514e2f44040d0557c94cdc4ace72b6754d35
Maffin <maftuna.nova@gmail.com>
2021-12-17 10:19:16 +0500

    box

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const box = document.querySelector('.box');
+const box = document.querySelectorAll('.box');
 
 box.forEach(item => {
     item.addEventListener('click', () => {
       
 
-        // item.classList.toggle('circle')
+        item.classList.toggle('circle')
         
-        if(item.classList.contains('circle')) {
-            item.classList.remove('circle')
-        }else {
-            item.classList.add('circle')
-        }
+        // if(item.classList.contains('circle')) {
+        //     item.classList.remove('circle')
+        // }else {
+        //     item.classList.add('circle')
+        // }
     })
 })
